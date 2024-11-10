d15f8d132fdd4d2ef6d0c21a03f5157da6ae30c7
Tajinder Singh Dhoot <tajindersinghdhoot@Singh-on-Air.local>
2024-11-10 18:25:08 -0500

    variables methods properties implemented

---
+++ PropertiesInC#/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Order
+    {
+        public string Item { get; }
+
+        private DateTime _date;
+        public DateTime Date
+        { 
+            get { return _date;}
+            set
+            {
+                if (value.Year == DateTime.Now.Year)
+                {
+                    _date = value;
+                }
+            }
+        }
+
+        public Order(string item, DateTime date)
+        {
+            Item = item;
+            Date = date;
+        }
+
+        public static void Main()
+        {
+            // Order order1 = new Order("laptop", DateTime.Now);
+
+            DateTime date = new DateTime(2023, 4, 15);
+            Order order1 = new Order("laptop", date);
+
+            Console.WriteLine(order1.Item);
+            Console.WriteLine(order1.Date);
+        }
+    }
+}
---
+++ Triangle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Triangle
+    {
+        private int @base;
+        private int height;
+        
+        public Triangle(int @base, int height)
+        {
+            this.@base = @base;
+            this.height = height;
+            
+        }
+        
+        public int CalculateArea()
+        {
+            Console.WriteLine(AsString());
+            return ((@base * height) / 2);
+            
+        }
+        
+        public string AsString()
+        {
+            return ("Base is " +@base+ ", height is " +height);
+        }
+
+        public static void Main() 
+        {
+            Triangle triangle1 = new Triangle(5, 10);
+            Console.WriteLine("Area of trianle is : " +triangle1.CalculateArea());
+        }
+    }
+}
