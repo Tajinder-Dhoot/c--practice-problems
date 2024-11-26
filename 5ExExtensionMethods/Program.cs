ef08b5849b801c00c69043e80725d381623d07d1
Tajinder Singh Dhoot <tajindersinghdhoot@Singh-on-Air.local>
2024-11-25 22:18:47 -0500

    extension method exercise

---
+++ 5ExExtensionMethods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Coding.Exercise
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<int> {1, 2, 3, 10, 5, 78, 3};
+            List<int> result = list.TakeEverySecond();
+            foreach (var item in result)
+            {
+                Console.Write(item + ", ");
+            }
+        }
+    }
+
+    public static class ListExtension
+    {
+        public static List<int> TakeEverySecond(this List<int> list)
+        {
+            List<int> result = [];
+            
+            for (var i = 0; i < list.Count; i+=2)
+            {
+                result.Add(list[i]);
+            }
+            return result;
+        }
+    }
+}
